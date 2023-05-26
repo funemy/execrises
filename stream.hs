0d767c8afd0dffc8b01b50c5d2a9be3d0b350edd
funemy <liyzunique@gmail.com>
2023-05-26 15:08:04 -0700

    minor

--- stream.hs
+++ stream.hs
@@ -1,6 +1,9 @@
 data St a = Mk a (St a)
     deriving (Show)
 
+instance Functor St where
+    fmap f (Mk h t) = Mk (f h) (fmap f t)
+
 hd :: St a -> a
 hd (Mk n s) = n
 
@@ -8,14 +11,17 @@ tl :: St a -> St a
 tl (Mk n s) = s
 
 tk :: Int -> St a -> [a]
-tk 0 _         = []
-tk n (Mk n' s) = n' : tk (n-1) s
+tk 0 _ = []
+tk n (Mk n' s) = n' : tk (n - 1) s
 
 ones :: St Int
 ones = Mk 1 ones
 
 ones' :: St Int
 ones' = Mk 1 (tl ones')
+
+nats :: St Int
+nats = Mk 0 (fmap (+ 1) nats)
 
 s :: St Bool
 s = if hd s then undefined else undefined
