47a348e884528d2093fd896718586cc940412725
funemy <liyzunique@gmail.com>
2023-05-26 21:22:58 -0700

    minor

--- stream.ml
+++ stream.ml
@@ -1,7 +1,9 @@
 type st = Mk of int * st
 
 let rec ones = Mk (1, ones)
-
+let rec map f (Mk (n, s)) = Mk (f n, map f s)
 let head (Mk (n, s)) = n
-
 let tail (Mk (n, s)) = s
+
+(* below will cause stackoverflow *)
+(* let twos = map succ ones *)
