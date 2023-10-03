030c09e7352c76e92817bf95ac9078c1c7ca3158
funemy <liyzunique@gmail.com>
2023-10-03 03:46:26 -0700

    recover the duality between mutu and comutu

--- cata.hs
+++ cata.hs
@@ -1,11 +1,15 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
 import Prelude hiding (succ)
+import Data.Bifunctor (Bifunctor, bimap)
 
 newtype Fix f = In { unIn :: f (Fix f) }
 
-cata :: Functor f => (f a -> a) -> Fix f -> a
+type Mu f = Fix f
 
+cata :: Functor f => (f a -> a) -> Mu f -> a
 cata alg = alg . fmap (cata alg) . unIn
 
 delta :: (a -> b) -> (a -> c) -> a -> (b, c)
@@ -13,7 +17,7 @@ delta f g x = (f x, g x)
 
 data NatF x = Zero | Succ x
 
-type Nat = Fix NatF
+type Nat = Mu NatF
 
 instance Functor NatF where
   fmap f Zero = Zero
@@ -64,7 +68,7 @@ mul n = cata (algMul n)
 ten :: Nat
 ten = two `mul` five
 
-para :: Functor f => (f (Fix f, a) -> a) -> Fix f -> a
+para :: Functor f => (f (Mu f, a) -> a) -> Mu f -> a
 para alg = snd . cata ((In . fmap fst) `delta` alg)
 -- para alg n = snd $ ((In . fmap fst) `delta` alg) $ fmap (cata ((In . fmap fst) `delta` alg)) (unIn n)
 -- para alg n = snd $ ((In . fmap fst) `delta` alg) $ fmap (cata ((In . fmap fst) `delta` alg)) Zero
@@ -98,6 +102,34 @@ fact' :: Nat -> Nat
 fact' = snd . cata algFact'
 
 -- TODO: cata to mutu
+
+-- This is the same definition as the paper
+mutu :: forall f a b . Functor f => (f (a,b) -> a) -> (f (a, b) -> b) -> (Mu f -> a, Mu f -> b)
+mutu f g = (fst . cata alg, snd . cata alg)
+  where
+    alg :: f (a, b) -> (a, b)
+    alg x = (f x, g x)
+
+-- can we generalize mutu?
+newtype Mu1 f g = In1 { unIn1 :: f (Mu1 f g) (Mu2 f g) }
+newtype Mu2 f g = In2 { unIn2 :: g (Mu1 f g) (Mu2 f g) }
+
+mutu' :: (Bifunctor f, Bifunctor g) => (f a b -> a) -> (g a b -> b) -> Mu1 f g -> Mu2 f g -> (a, b)
+mutu' alg1 alg2 m1 m2 = (x m1, y m2)
+  where
+    x = alg1 . bimap x y . unIn1
+    y = alg2 . bimap x y . unIn2
+
+-- comutu
+newtype Nu1 f g = UnOut1 { out1 :: f (Nu1 f g) (Nu2 f g) }
+newtype Nu2 f g = UnOut2 { out2 :: g (Nu1 f g) (Nu2 f g) }
+
+comutu :: (Bifunctor f, Bifunctor g) => (a -> f a b) -> (b -> g a b) -> a -> b -> (Nu1 f g, Nu2 f g)
+comutu coalg1 coalg2 a b = (x a, y b)
+  where
+    x = UnOut1 . bimap x y . coalg1
+    y = UnOut2 . bimap x y . coalg2
+
 
 main :: IO ()
 main = putStrLn "Hello World"
