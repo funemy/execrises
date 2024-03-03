fc1c63104dbf66f4a30b02a2a73617b26509b4be
funemy <liyzunique@gmail.com>
2024-03-02 20:24:08 -0800

    minor update

--- contra.hs
+++ contra.hs
@@ -8,14 +8,19 @@ import Prelude hiding (Functor (..))
 class Functor f where
     fmap :: (a -> b) -> f a -> f b
 
-instance Functor ((->) r) where
+-- For function i -> o (i for input, o for ouput)
+-- i -> _ is a covariant functor
+instance Functor ((->) i) where
     fmap f g = f . g
 
 class Contra f where
     cmap :: (a -> b) -> f b -> f a
 
-newtype Op r a = Op { getOp :: a -> r }
+-- Op means "Opposite"
+-- This is essentially the same thing as the C^op categories you see in category theory.
+-- Op o i == i -> o
+newtype Op o i = Op { getOp :: i -> o }
 
-instance Contra (Op r) where
+instance Contra (Op o) where
     cmap f g = Op $ getOp g . f
 
